9de8663193ea29d95185017aed7a6bf571f79211
Andrzej Pronobis <a.pronobis@gmail.com>
2015-07-26 13:01:32 -0700

    Mark installed

--- shell/install_module_footer.sh
+++ shell/install_module_footer.sh
@@ -1,5 +1,9 @@
 # -*- mode: sh -*-
 
+# Mark that installation has been completed
+cd ${DOT_MODULE_DIR}
+git rev-parse HEAD > ${DOT_MODULE_DIR}/installed
+
 # Unset "local" vars
 unset DOT_MODULE_DIR
 
