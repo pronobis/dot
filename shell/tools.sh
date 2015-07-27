a079be942129a83e52cab4bbc177bd0f2be71b2a
Andrzej Pronobis <a.pronobis@gmail.com>
2015-07-26 17:34:21 -0700

    .

--- shell/tools.sh
+++ shell/tools.sh
@@ -96,8 +96,7 @@ print_error()
 ## -------------------------------------------------------------
 yes_no_question()
 {
-    local question=$1
-    printf "${BOLD}${YELLOW}$question ${WHITE}(${LIGHT_GREEN}y${WHITE}/${LIGHT_RED}n${WHITE}):${NO_FORMAT} "
+    printf "${BOLD}${YELLOW}$1 ${WHITE}(${LIGHT_GREEN}y${WHITE}/${LIGHT_RED}n${WHITE}):${NO_FORMAT} "
     old_stty_cfg=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg
     if echo "$answer" | grep -iq "^y" ;then
