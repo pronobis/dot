fc7e648105004a9320f7c809ccc8a128c263cb8e
Andrzej Pronobis <a.pronobis@gmail.com>
2015-08-06 19:02:16 -0700

    Updated default module example

--- modules/00_defaults/install.sh
+++ modules/00_defaults/install.sh
@@ -1,25 +1,51 @@
 #!/bin/sh
 
 dot_shell=$(cd "${0%/*}/../../shell" && pwd); . "$dot_shell/install_module_header.sh"
-check_virtualenv  # Check for virtualenv
+dot_check_root # Check if we run as root
+# dot_check_ubuntu  # Are we on Ubuntu?
+dot_check_virtualenv  # Check for virtualenv
 
 
 ## -------------------------------------------------------------
 ## Installation
 ## -------------------------------------------------------------
 
-## -------------------------------------------------------------
-print_header "Creating links to binaries"
-# dot_link_bin $DOT_MODULE_DIR "scripts/display-manager"
-print_status "Done!"
+print_warning "This is just an example that does nothing!"
 
 ## -------------------------------------------------------------
-print_header "Installing user-local config files"
-# dot_link_config $DOT_MODULE_DIR ".selected_editor"
-# dot_append_to_config $DOT_MODULE_DIR ".ssh/config" "# dot-module-default configuration begins here" "# dot-module-default configuration ends here"
-# dot_fill_config $DOT_MODULE_DIR ".config/mc/ini" # Fill the ini file with $HOME path
-# dot_copy_config $DOT_MODULE_DIR ".aspell.en.pws"
-print_status "Done!"
+# print_header "Installing some packages"
+# if dot_check_packages build-essential cmake cmake-curses-gui ccache
+# then
+#     print_status "All packages are already installed."
+# else
+#     if yes_no_question "Install packages: $DOT_NOT_INSTALLED?"
+#     then
+#         dot_install_packages $DOT_NOT_INSTALLED
+#         print_status "Done!"
+#     fi
+# fi
+
+
+## -------------------------------------------------------------
+# print_header "Creating links to binaries"
+# dot_link_bin $DOT_MODULE_DIR "scripts/my-binary"
+# print_status "Done!"
+
+
+## -------------------------------------------------------------
+# print_header "Installing user-local config files"
+# dot_link_config $DOT_MODULE_DIR ".local/share/icons/*"
+# dot_copy_config $DOT_MODULE_DIR ".kde/share/config/digikamrc"
+# print_status "Done!"
+
+
+## -------------------------------------------------------------
+# print_header "Installing system-wide config files"
+# dot_link_config_sys $DOT_MODULE_DIR "etc/default/crda"
+# dot_copy_config_sys $DOT_MODULE_DIR "usr/lib/pm-utils/power.d/powertop_toggables"
+# dot_fill_config_sys $DOT_MODULE_DIR "usr/share/sddm/scripts/Xsetup"
+# Done
+# print_status "Done!"
 
 
 ## -------------------------------------------------------------
