5d458d5f2880fa85926cf8a2f3947675bf09ce50
Andrzej Pronobis <a.pronobis@gmail.com>
2017-03-06 21:27:24 -0800

    Added a smarted cdot function

--- modules/00_defaults/shell/setup-interactive.sh
+++ modules/00_defaults/shell/setup-interactive.sh
@@ -4,4 +4,20 @@
 ## sessions for any POSIX shell.
 ## ----------------------------------------------------------
 
-alias cdot='cd $DOT_DIR/modules'
+cdot()
+{
+    local name="$1"
+    if [ -n "$name" ]
+    then
+        # Find first module matching name
+        for i in `ls $DOT_DIR/modules | sort`
+        do
+            if [ "${i#$name}" != "$i" ]
+            then
+                cd $DOT_DIR/modules/$i
+            fi
+        done
+    else
+        cd $DOT_DIR/modules
+    fi
+}
