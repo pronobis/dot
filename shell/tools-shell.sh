b9c52234575c625ff8a9bbe535194ca970776709
Andrzej Pronobis <a.pronobis@gmail.com>
2017-03-07 14:32:22 -0800

    Moved functions visible in shell to tools-shell.sh

---
+++ shell/tools-shell.sh
@@ -0,0 +1,46 @@
+# -*- mode: sh -*-
+
+## ----------------------------------------------------------
+## Functions available in the shell
+## ----------------------------------------------------------
+
+# Include guard
+[ -n "$DOT_SETUP_TOOLS_SHELL" ] && return || readonly DOT_SETUP_TOOLS_SHELL=1
+
+
+# Get a list of downloaded modules
+# Return:
+#   $DOT_MODULES - a list of modules (separated by colons)
+_dot_get_modules()
+{
+    DOT_MODULES=""
+    local p=""
+    for i in `ls $DOT_DIR/modules | sort`
+    do
+        p="$DOT_DIR/modules/$i"
+        if [ -d "$p" ] && [ $(basename "$p") != "temp_clone" ] && [ -f $p/.git/config ]
+        then
+            DOT_MODULES=${DOT_MODULES:+${DOT_MODULES}:}$i
+        fi
+    done
+}
+
+
+# Return names of modules containing the argument
+# Args:
+#   $1 - Part of module name
+# Return:
+#   $DOT_MATCHING_MODULES - a list of matching modules (separated by colons)
+_dot_get_modules_matching_name()
+{
+    DOT_MATCHING_MODULES=""
+    local IFS=':'
+    for i in $DOT_MODULES
+    do
+        if [ "${i#*$name}" != "$i" ]
+        then
+            DOT_MATCHING_MODULES=${DOT_MATCHING_MODULES:+${DOT_MATCHING_MODULES}:}$i
+        fi
+    done
+
+}
