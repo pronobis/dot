af7f3cd4f4164ae2c99822d66b248ede01df5b52
Andrzej Pronobis <a.pronobis@gmail.com>
2017-03-06 21:33:53 -0800

    Added comment and fixed cdot

--- modules/00_defaults/shell/setup-interactive.sh
+++ modules/00_defaults/shell/setup-interactive.sh
@@ -4,6 +4,10 @@
 ## sessions for any POSIX shell.
 ## ----------------------------------------------------------
 
+# Go to the module directory indicated by part of its name.
+# If no argument is given, go to the directory containing all modules.
+# Args:
+#   $1 - Part of the module name
 cdot()
 {
     local name="$1"
@@ -12,9 +16,10 @@ cdot()
         # Find first module matching name
         for i in `ls $DOT_DIR/modules | sort`
         do
-            if [ "${i#$name}" != "$i" ]
+            if [ "${i/$name/}" != "$i" ]
             then
                 cd $DOT_DIR/modules/$i
+                break
             fi
         done
     else
