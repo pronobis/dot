70d10fa4534bb733297bbb3449fb28f140e05ae3
Andrzej Pronobis <a@pronobis.pro>
2020-02-21 21:38:27 +0100

    New variables for internal module use

--- shell/init_module_header.sh
+++ shell/init_module_header.sh
@@ -9,8 +9,12 @@ if [ -z "$DOT_DIR" ]
 then
    export DOT_DIR=$( cd "$DOT_MODULE_DIR/../.." && pwd )
 fi
-TMP_DIR="$DOT_MODULE_DIR/tmp"
-OPT_DIR="$DOT_MODULE_DIR/opt"
+export TMP_DIR="$DOT_MODULE_DIR/tmp"
+export OPT_DIR="$DOT_MODULE_DIR/opt"
+export CONFIG_DIR="$DOT_MODULE_DIR/config"
+export CONFIG_SYS_DIR="$DOT_MODULE_DIR/config-sys"
+export SCRIPTS_DIR="$DOT_MODULE_DIR/scripts"
+export BIN_DIR="$DOT_MODULE_DIR/bin"
 
 # Import tools
 . "$DOT_DIR/shell/tools.sh"
--- shell/install_module_footer.sh
+++ shell/install_module_footer.sh
@@ -7,6 +7,12 @@ $DOT_GIT rev-parse HEAD > ${TMP_DIR}/installed
 
 # Unset exported "local" vars
 unset DOT_MODULE_DIR
+unset TMP_DIR
+unset OPT_DIR
+unset CONFIG_DIR
+unset CONFIG_SYS_DIR
+unset SCRIPTS_DIR
+unset BIN_DIR
 
 # Footer
 print_main_module_footer
